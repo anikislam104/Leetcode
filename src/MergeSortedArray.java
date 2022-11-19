6a597e4b832678dca94958ba66b4dfdbc3bbf674
Anik Islam Pantha <1705104@ugrad.cse.buet.ac.bd>
2022-11-19 20:58:07 +0600

    88. Merge Sorted Array

--- src/MergeSortedArray.java
+++ src/MergeSortedArray.java
@@ -2,7 +2,7 @@ class MergeSortedArrayHelper {
     int partition(int[] arr,int low,int high){
         int pivot=arr[high];
         int i=low-1;
-        for (int j=low;j<high-1;j++){
+        for (int j=low;j<=high-1;j++){
             if(arr[j]<pivot){
                 i++;
                 int temp=arr[i];
@@ -23,8 +23,23 @@ class MergeSortedArrayHelper {
         }
     }
     public void merge(int[] nums1, int m, int[] nums2, int n) {
-
+        for(int i=0;i<n;i++){
+            nums1[i+m]=nums2[i];
+        }
+        for(int i=0;i<m+n;i++){
+//            System.out.print(nums1[i]);
+        }
+        quick_sort(nums1,0,m+n-1);
+        for(int i=0;i<m+n;i++){
+            System.out.println(nums1[i]);
+        }
     }
 }
 public class MergeSortedArray {
+    public static void main(String[] args) {
+        int[] nums1={1,2,3,0,0,0};
+        int[] nums2={2,5,6};
+        MergeSortedArrayHelper mergeSortedArrayHelper=new MergeSortedArrayHelper();
+        mergeSortedArrayHelper.merge(nums1,3,nums2,3);
+    }
 }
